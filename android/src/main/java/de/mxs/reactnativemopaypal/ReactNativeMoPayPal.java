ee6e08f0166e988cb0c15bd0115e7acb8e4fa921
Moritz Möller <mm@mxs.de>
2022-04-04 20:04:58 +0200

    ..

--- android/src/main/java/de/mxs/reactnativemopaypal/ReactNativeMoPayPal.java
+++ android/src/main/java/de/mxs/reactnativemopaypal/ReactNativeMoPayPal.java
@@ -1,11 +1,23 @@
 package de.mxs.reactnativemopaypal;
 
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.paypal.checkout.PayPalCheckout;
+import com.paypal.checkout.config.CheckoutConfig;
+import com.paypal.checkout.config.Environment;
+import com.paypal.checkout.createorder.CurrencyCode;
+import com.paypal.checkout.createorder.OrderIntent;
+import com.paypal.checkout.order.Amount;
+import com.paypal.checkout.order.Order;
+import com.paypal.checkout.order.PurchaseUnit;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Objects;
 
 import javax.annotation.Nonnull;
 
@@ -19,6 +31,77 @@ public class ReactNativeMoPayPal extends ReactContextBaseJavaModule {
     public @Nonnull
     String getName() {
         return "ReactNativeMoPayPal";
+    }
+
+    @SuppressWarnings("unused")
+    @ReactMethod
+    public void test(ReadableMap args, Promise promise) {
+        Log.i("PayPal", "start");
+        Log.i("PayPal", "args=" + args);
+
+        PayPalCheckout.setConfig(
+            new CheckoutConfig(
+                Objects.requireNonNull(getReactApplicationContext().getCurrentActivity()).getApplication(),
+                Objects.requireNonNull(args.getString("clientID")),
+                Environment.valueOf(Objects.requireNonNull(args.getString("environment")).toUpperCase()),
+                getReactApplicationContext().getPackageName() + "://paypalpay"
+            )
+        );
+
+        PurchaseUnit purchaseUnit = new PurchaseUnit.Builder()
+            .amount(
+                new Amount.Builder()
+                    .currencyCode(CurrencyCode.EUR)
+                    .value("1.22")
+                    .build()
+            )
+//            .customId("custom-id")
+//            .invoiceId("invoice-id")
+            .build();
+
+        Order order = new Order.Builder()
+            .intent(OrderIntent.AUTHORIZE)
+            .purchaseUnitList(Arrays.asList(purchaseUnit))
+            .build();
+
+        Log.i("PayPal", "order=" + order);
+
+        PayPalCheckout.registerCallbacks(
+            approval -> {
+                Log.i("PayPal", "onApprove " + approval);
+            },
+            null,
+            () -> {
+                Log.i("PayPal", "onCancel");
+            },
+            errorInfo -> {
+                Log.i("PayPal", "onError " + errorInfo);
+            }
+        );
+
+        Log.i("PayPal", "startCheckout");
+        PayPalCheckout.startCheckout(createOrderActions -> {
+            Log.i("PayPal", "create " + createOrderActions);
+            createOrderActions.create(order, s -> {
+                Log.i("PayPal", "onCreated " + s);
+            });
+        });
+
+
+//        val createOrderRequest =
+//            CreateOrderRequest(
+//                orderIntent = selectedOrderIntent,
+//                userAction = selectedUserAction,
+//                shippingPreference = selectedShippingPreference,
+//                currencyCode = currencyCode,
+//                createdItems = createdItems
+//            )
+//        val order = createOrderUseCase.execute(createOrderRequest)
+
+//        PayPalCheckout.startCheckout(...)
+//        const val PAYPAL_CLIENT_ID = "YOUR-CLIENT-ID-HERE"
+//        const val PAYPAL_SECRET = "ONLY-FOR-QUICKSTART-DO-NOT-INCLUDE-SECRET-IN-CLIENT-SIDE-APPLICATIONS"
+//        com.paypal.checkoutsamples://paypalpay
     }
 
 }
--- ios/ReactNativeMoPayPal/ReactNativeMoPayPal.m
+++ ios/ReactNativeMoPayPal/ReactNativeMoPayPal.m
@@ -3,6 +3,57 @@
 
 @import PayPalCheckout;
 
+
+PPCPurchaseUnitAmount* PPCPurchaseUnitAmountFromDictionary(NSDictionary* _Nonnull rs) {
+    // @TODO: remaining fields, currency
+    return [[PPCPurchaseUnitAmount alloc]
+      initWithCurrencyCode:PPCCurrencyCodeEur
+      value:[rs[@"value"] stringValue]
+      breakdown:nil
+    ];
+}
+
+PPCPurchaseUnit* PPCPurchaseUnitFromDictionary(NSDictionary* _Nonnull rs) {
+    // @TODO: remaining fields
+    NSLog(@"test [%@]", [rs[@"referenceId"] stringValue]);
+    return [[PPCPurchaseUnit alloc]
+        initWithAmount:PPCPurchaseUnitAmountFromDictionary(rs[@"amount"])
+        referenceId:nil // string
+        payee:nil // PPCPurchaseUnitPayee
+        paymentInstruction:nil // PPCPurchaseUnitPaymentInstruction
+        purchaseUnitDescription:nil // string
+        customId:nil // string
+        invoiceId:nil // string
+        softDescriptor:nil // string
+        items:nil // (NSArray<PPCPurchaseUnitItem *> * _Nullable)
+        shipping:nil // PPCPurchaseUnitShipping
+    ];
+}
+
+PPCOrderRequest* PPCOrderRequestFromDictionary(NSDictionary* _Nonnull rs) {
+    NSMutableArray<PPCPurchaseUnit*>* purchaseUnits = [NSMutableArray new];
+    for (NSDictionary* tmp in rs[@"purchaseUnits"]) {
+        [purchaseUnits addObject:PPCPurchaseUnitFromDictionary(tmp)];
+    }
+    // @TODO: fields
+    return [[PPCOrderRequest alloc]
+        initWithIntent:PPCOrderIntentCapture // PPCOrderIntentCapture, PPCOrderIntentAuthorize
+        purchaseUnits:purchaseUnits
+        processingInstruction:PPCOrderProcessingInstructionNone
+        payer:nil
+        applicationContext:nil
+    ];
+}
+
+PPCEnvironment PPCEnvironmentFromString(NSString* _Nonnull str) {
+    NSDictionary* values = @{
+        @"live": @(PPCEnvironmentLive),
+        @"sandbox": @(PPCEnvironmentSandbox),
+        @"stage": @(PPCEnvironmentStage),
+    };
+    return [values[str] intValue];
+}
+
 // RCTEventEmitter ?
 
 @interface ReactNativeMoPayPal : NSObject <RCTBridgeModule>
@@ -22,96 +73,77 @@ RCT_EXPORT_MODULE()
 }
 
 - (NSArray<NSString*>*)supportedEvents {
-  return @[ ];
+    return @[ ];
 }
 
-RCT_EXPORT_METHOD(testPayPal:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
-  NSLog(@"testPayPal");
-
-  // @TODO: check client id ?
-  // @TODO: arguments object
-  // @TODO: need to setup config in appdelegate maybe?
-  // @TODO:
-
-  PPCheckoutConfig* config = [[PPCheckoutConfig alloc]
-    initWithClientID:@"todo..." // sandbox client_id
-    returnUrl:@"own-budnle-id://paypalpay"
-    createOrder:nil
-    onApprove:nil
-    onShippingChange:nil
-    onCancel:nil
-    onError:^(PPCErrorInfo * _Nonnull error) {
-      NSLog(@"onError %@", error);
-    }
-    environment:PPCEnvironmentSandbox
-  ];
-
-  [PPCheckout setConfig:config];
-
-  UIViewController* currentViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
-
-  [PPCheckout
-    startWithPresentingViewController:currentViewController
-    createOrder:^(PPCCreateOrderAction * _Nonnull arg) {
-      PPCPurchaseUnitAmount* purchaseUnitAmount = [[PPCPurchaseUnitAmount alloc]
-        initWithCurrencyCode:PPCCurrencyCodeEur
-        value:@"1.11"
-        breakdown:nil
-      ];
-      PPCPurchaseUnit* purchaseUnit = [[PPCPurchaseUnit alloc]
-        initWithAmount:purchaseUnitAmount
-        referenceId:nil
-        payee:nil
-        paymentInstruction:nil
-        purchaseUnitDescription:nil
-        customId:nil
-        invoiceId:nil
-        softDescriptor:nil
-        items:nil // (NSArray<PPCPurchaseUnitItem *> * _Nullable)
-        shipping:nil
-      ];
-      PPCOrderRequest* orderRequest = [[PPCOrderRequest alloc]
-        initWithIntent:PPCOrderIntentCapture
-        purchaseUnits:@[purchaseUnit]
-        processingInstruction:PPCOrderProcessingInstructionNone
-        payer:nil
-        applicationContext:nil
-      ];
-      NSLog(@"orderRequest %@", orderRequest);
-      [arg
-        createWithOrder:orderRequest
-        completion:^(NSString * _Nullable arg2) {
-          NSLog(@"completion %@", arg);
+RCT_EXPORT_METHOD(test:(NSDictionary*)args resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
+    NSLog(@"paypal test");
+   
+    NSLog(@"args %@", args);
+    
+    NSString* returnUrl = [NSString stringWithFormat:@"%@://paypalpay", [[NSBundle mainBundle] bundleIdentifier]];
+    // @TODO: do we handle this?
+    
+    PPCheckoutConfig* config = [[PPCheckoutConfig alloc]
+        initWithClientID:[args[@"clientID"] stringValue]
+        returnUrl:returnUrl
+        createOrder:nil
+        onApprove:nil
+        onShippingChange:nil
+        onCancel:nil
+        onError:^(PPCErrorInfo * _Nonnull error) {
+            // @TODO reject?
+            NSLog(@"PPCheckoutConfig onError %@", error);
         }
-      ];
-    }
-    onApprove:^(PPCApproval * _Nonnull arg) {
-      // called.
-      NSLog(@"onApprove %@", arg);
-      resolve(@{
-        @"result": @"approved",
-        @"t1": arg.data.payerID,
-        @"t2": arg.data.ecToken,
-        @"t3": arg.data.paymentID,
-      });
-    }
-    onShippingChange:nil
-    onCancel:^{
-      // called.
-      NSLog(@"onCancel");
-      resolve(@{
-        @"result": @"cancelled",
-      });
-    }
-    onError:^(PPCErrorInfo * _Nonnull error) {
-      NSLog(@"onError %@", error);
-      resolve(@{
-        @"result": @"error",
-        @"reason": error.reason,
-        @"error": error.error.description,
-      });
-    }
-  ];
+        environment:PPCEnvironmentFromString(args[@"environment"])
+    ];
+    [PPCheckout setConfig:config];
+
+    UIViewController* currentViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    [PPCheckout
+        startWithPresentingViewController:currentViewController
+        createOrder:^(PPCCreateOrderAction * _Nonnull createOrderAction) {
+            PPCOrderRequest* orderRequest = PPCOrderRequestFromDictionary(args[@"orderRequest"]);
+            NSLog(@"orderRequest %@", orderRequest);
+
+            [createOrderAction
+                createWithOrder:orderRequest
+                completion:^(NSString * _Nullable arg2) {
+                    NSLog(@"completion %@", arg2);
+                    // @TODO: resolve?
+                }
+            ];
+
+        }
+        onApprove:^(PPCApproval * _Nonnull arg) {
+            // called.
+            NSLog(@"onApprove %@", arg);
+            NSLog(@"onApprove data %@", arg.data);
+            resolve(@{
+                @"result": @"approved",
+//                @"t1": arg.data.payerID,
+//                @"t2": arg.data.ecToken,
+//                @"t3": arg.data.paymentID,
+            });
+        }
+        onShippingChange:nil
+        onCancel:^{
+            // called.
+            NSLog(@"onCancel");
+            resolve(@{
+                @"result": @"cancelled",
+            });
+        }
+        onError:^(PPCErrorInfo * _Nonnull error) {
+            NSLog(@"onError %@", error);
+            resolve(@{
+            @"result": @"error",
+            @"reason": error.reason,
+            @"error": error.error.description,
+            });
+        }
+    ];
 }
 
 @end
